ac774c5643f460e4485e0d4c82a2e7d0013069b2
nielsdelestinne <nielsdelestinne@gmail.com>
2018-06-05 11:11:57 +0200

    Swapped deprecated api for new one

--- gameoflife-backend/src/test/java/be/switchfully/gameoflife/backend/UnitTest.java
+++ gameoflife-backend/src/test/java/be/switchfully/gameoflife/backend/UnitTest.java
@@ -1,7 +1,7 @@
 package be.switchfully.gameoflife.backend;
 
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public abstract class UnitTest {
